863a07c061f93544e24891af66ffa872bc010e32
Alexander-Guacan <adguacan@espe.edu.ec>
2026-05-12 09:03:25 -0500

    Change login view title

--- CLICON_CONVUNI_RESTFUL_JAVA_GR01/src/main/java/ec/edu/monster/views/LoginView.java
+++ CLICON_CONVUNI_RESTFUL_JAVA_GR01/src/main/java/ec/edu/monster/views/LoginView.java
@@ -13,6 +13,7 @@ public class LoginView {
         System.out.print("\033[H\033[2J");
         System.out.flush();
 
+        System.out.println("=== CLICON RESTFUL - JAVA GR01 ===");
         System.out.println("=== INICIO DE SESIÓN ===");
         System.out.print("Usuario: ");
         String user = scanner.nextLine();
